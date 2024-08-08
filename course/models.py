63371d598aefc89f96261f162ad508965dac981c
Royd0101 <royd@classify.com.ph>
2024-08-08 08:27:30 +0800

    activity

--- course/models.py
+++ course/models.py
@@ -16,9 +16,7 @@ class Course(models.Model):
 class Section(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     section_name = models.CharField(max_length=100)
-    subjects = models.ManyToManyField(Subject)
-    assign_teacher = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    
+    subjects = models.ManyToManyField(Subject) 
 
     def __str__(self):
         return self.section_name
--- subject/models.py
+++ subject/models.py
@@ -12,6 +12,7 @@ class Subject(models.Model):
     subject_name = models.CharField(max_length=100)
     subject_short_name = models.CharField(max_length=10, null= True, blank=True)
     subject_photo = models.ImageField(upload_to=get_upload_path, null=True, blank=True)
+    assign_teacher = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.subject_name
