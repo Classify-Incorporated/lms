58a1e93218afc0098076e7463e57d90c5b948d6c
Royd0101 <royd@classify.com.ph>
2024-08-19 16:46:41 +0800

    scorm

--- module/models.py
+++ module/models.py
@@ -3,9 +3,16 @@ from subject.models import Subject
 import os
 import uuid
 
+
 def get_upload_file(instance, filename):
     filename = f"{uuid.uuid4()}{os.path.splitext(filename)[1]}"
     return os.path.join('module', filename)
+
+
+def get_scorm_upload_path(instance, filename):
+    filename = f"{uuid.uuid4()}{os.path.splitext(filename)[1]}"
+    return os.path.join('scormPackages', filename)
+
 
 # Create your models here.
 class Module(models.Model):
@@ -16,3 +23,16 @@ class Module(models.Model):
     def __str__(self):
         return self.file_name
     
+class SCORMPackage(models.Model):
+    package_name = models.CharField(max_length=100)
+    pptx_link = models.URLField(max_length=200, null=True, blank=True)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.package_name
+
+    # You can keep the save method as is if you plan to add SCORM conversion later
+    # Or remove the conversion logic if it's not needed for now
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        # Placeholder for future SCORM conversion logic
