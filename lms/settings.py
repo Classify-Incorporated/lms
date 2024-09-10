b2c1594c73d335e05d76f3f9dafa936e6dc8495f
LouieJae <louie@classify.com.ph>
2024-09-10 10:47:25 +0800

    Subjects New Design

--- lms/settings.py
+++ lms/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.microsoft',
+    'django_summernote',
 ]
 
 MIDDLEWARE = [
--- message/admin.py
+++ message/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Message, MessageReadStatus
+from django_summernote.admin import SummernoteModelAdmin
 # Register your models here.
+class MessageAdmin(SummernoteModelAdmin):
+    summernote_fields = ('body',)  # Apply Summernote to 'body' field
 
 admin.site.register(Message)
 admin.site.register(MessageReadStatus)
